194cf012292447b164f8e244d7ba5a8c3012740d
Real <swrely@qq.com>
2024-04-16 23:36:31 +0800

    feat[basicSyntax]: Inputs and outputs: basic operations and files

---
+++ basic_syntax/input_and_output/main.py
@@ -0,0 +1,19 @@
+name = input('your name: ')
+gender = input('you are a boy(y/n)? ')
+
+# ###### 输入 ######
+# your name: Real
+# you are a boy(y/n)? y
+
+welcome_str = 'Welcome to the matrix {prefix} {name}.'
+welcome_dic = {
+    'prefix': 'Mr.' if gender == 'y' else 'Mrs',
+    'name': name
+}
+
+print('authorizing...')
+print(welcome_str.format(**welcome_dic))
+
+# ########## 输出 ##########
+# authorizing...
+# Welcome to the matrix Mr. Real.
