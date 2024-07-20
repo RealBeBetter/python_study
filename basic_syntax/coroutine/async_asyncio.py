1d69e1282fcf5c956e7ee1e8065591beb0a38005
Real <swrely@qq.com>
2024-07-20 19:41:58 +0800

    feat[basicSyntax]: Add the use of asyncio

---
+++ basic_syntax/coroutine/async_asyncio.py
@@ -0,0 +1,45 @@
+import asyncio
+import string
+import time
+
+import aiohttp
+
+
+async def download_one(url: string):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            print('Read {} from {}'.format(resp.content_length, url))
+
+
+async def download_all(sites: list):
+    tasks = [download_one(site) for site in sites]
+    await asyncio.gather(*tasks)
+
+
+def main():
+    sites = [
+        'https://en.wikipedia.org/wiki/Portal:Arts',
+        'https://en.wikipedia.org/wiki/Portal:History',
+        'https://en.wikipedia.org/wiki/Portal:Society',
+        'https://en.wikipedia.org/wiki/Portal:Biography',
+        'https://en.wikipedia.org/wiki/Portal:Mathematics',
+        'https://en.wikipedia.org/wiki/Portal:Technology',
+        'https://en.wikipedia.org/wiki/Portal:Geography',
+        'https://en.wikipedia.org/wiki/Portal:Science',
+        'https://en.wikipedia.org/wiki/Computer_science',
+        'https://en.wikipedia.org/wiki/Python_(programming_language)',
+        'https://en.wikipedia.org/wiki/Java_(programming_language)',
+        'https://en.wikipedia.org/wiki/PHP',
+        'https://en.wikipedia.org/wiki/Node.js',
+        'https://en.wikipedia.org/wiki/The_C_Programming_Language',
+        'https://en.wikipedia.org/wiki/Go_(programming_language)'
+    ]
+
+    start_time = time.perf_counter()
+    asyncio.run(download_all(sites))
+    end_time = time.perf_counter()
+    print(f'Download {len(sites)} sites in {end_time - start_time} seconds')
+
+
+if __name__ == "__main__":
+    main()
